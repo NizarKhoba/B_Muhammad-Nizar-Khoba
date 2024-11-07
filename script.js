e2a37bc9b3298e60ab73dca9a8ea4b802ec7e3de
NizarKhoba <nizarkhoba121@gmail.com>
2024-11-07 22:58:35 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// Menggunakan var
+var x = 10;
+document.write("<p>Nilai x (dengan var): " + x + "</p>");
+
+// Menggunakan let
+let y = 20;
+document.write("<p>Nilai y (dengan let): " + y + "</p>");
+
+// Menggunakan const
+const z = 30;
+document.write("<p>Nilai z (dengan const): " + z + "</p>");
+ 
+// Tipe Data String
+let nama = "Muhammad Nizar";
+console.log("Nama:", nama);
+alert("Nama: " + nama);
+
+// Tipe Data Object
+let mahasiswa = {
+   nama: "Nizar",
+   jurusan: "Sistem Informasi",
+   universitas: "Universitas Negeri Surabaya"
+};
+console.log("Data Mahasiswa:", mahasiswa);
+alert("Data Mahasiswa: Nama - " + mahasiswa.nama + ", Jurusan - " + mahasiswa.jurusan + ", Universitas - " + mahasiswa.universitas);
